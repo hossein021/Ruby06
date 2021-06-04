f37676259081103e0d46e03fd714c8b81e3fe7b3
Hossein <hosseinsalmani1373@gmail.com>
2021-06-04 12:42:46 +0430

    Strings

---
+++ main11.rb
@@ -0,0 +1,37 @@
+# puts "Hello"
+
+str1 = "Ruby"
+str2 = "Language"
+# puts str1
+# puts str2
+
+puts "This is: #{str1}"
+
+x = 8
+puts "This is: #{x} is my Size"
+
+mystr = "Ali"
+puts mystr
+
+mystr2 = String.new "ALI"
+puts mystr2
+
+newstr = "This is Ruby Language, is my favorite"
+puts newstr['is']
+puts newstr[5]
+puts newstr[12]
+puts newstr[18]
+puts newstr[-1]
+puts newstr[-2]
+
+puts newstr[2, 10]
+puts newstr[2..18]
+
+new = "this
+is
+ruby
+language"
+puts new
+
+puts %/ali reza
+sara mahdi/
